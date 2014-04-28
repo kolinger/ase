3d9f58c60b81b8557dfd02430e7dcb7fc80bb969
Tomáš Kolinger <tomas@kolinger.name>
2014-04-28 20:31:15 +0200

    speed up platform start (parallel agent registration)

--- src/main/java/cz/uhk/fim/ase/platform/StaticPlatform.java
+++ src/main/java/cz/uhk/fim/ase/platform/StaticPlatform.java
@@ -10,10 +10,34 @@ import cz.uhk.fim.ase.model.impl.AgentEntityImpl;
 public class StaticPlatform extends Platform {
 
     protected void setup() {
-        Integer max = ServiceLocator.getConfig().environment.agentsCount / 2;
-        for (Integer count = 1; count <= max; count++) {
-            addAgent(new TestAgent1(AgentEntityImpl.create("1")));
-            addAgent(new TestAgent2(AgentEntityImpl.create("2")));
+        Thread[] threads = new Thread[10];
+        final Integer max = ServiceLocator.getConfig().environment.agentsCount / 2 / 10;
+        for (int index = 0; index < 10; index++) {
+            threads[index] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (Integer count = 1; count <= max; count++) {
+                        addAgent(new TestAgent1(AgentEntityImpl.create("1")));
+                        addAgent(new TestAgent2(AgentEntityImpl.create("2")));
+                    }
+                }
+            });
+            threads[index].start();
+        }
+
+        boolean done = false;
+        while (!done) {
+            done = true;
+            for (int index = 0; index < 10; index++) {
+                if (threads[index].isAlive()) {
+                    done = false;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // ignore
+                    }
+                }
+            }
         }
     }
 }
