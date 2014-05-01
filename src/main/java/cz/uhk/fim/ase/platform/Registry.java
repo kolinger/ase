94ab0979c7907712a1fc2fd610550ae58ddfcd50
Tomáš Kolinger <tomas@kolinger.name>
2014-05-01 16:35:09 +0200

    improved registry (added more features)

--- src/main/java/cz/uhk/fim/ase/platform/Registry.java
+++ src/main/java/cz/uhk/fim/ase/platform/Registry.java
@@ -20,20 +20,43 @@ public class Registry {
     private List<AgentEntity> localIndex = new ArrayList<>();
     private List<AgentEntity> foreignIndex = new ArrayList<>();
     private Map<String, List<AgentEntity>> typeIndex = new HashMap<>();
+    private Map<String, List<AgentEntity>> typeLocalIndex = new HashMap<>();
+    private Map<String, List<AgentEntity>> typeForeignIndex = new HashMap<>();
 
     public void register(AgentEntity agent) {
         synchronized (agents) {
             if (!agents.contains(agent)) {
+                List<AgentEntity> agentsList;
+
                 // local and foreign index
                 if (agent.getNode().equals(ServiceLocator.getConfig().system.myAddress)) {
                     localIndex.add(agent);
+
+                    // type index
+                    String type = agent.getProperties().get("type");
+                    if (typeLocalIndex.containsKey(type)) {
+                        agentsList = typeLocalIndex.get(type);
+                    } else {
+                        agentsList = new ArrayList<>();
+                        typeLocalIndex.put(type, agentsList);
+                    }
+                    agentsList.add(agent);
                 } else {
                     foreignIndex.add(agent);
+
+                    // type index
+                    String type = agent.getProperties().get("type");
+                    if (typeForeignIndex.containsKey(type)) {
+                        agentsList = typeForeignIndex.get(type);
+                    } else {
+                        agentsList = new ArrayList<>();
+                        typeForeignIndex.put(type, agentsList);
+                    }
+                    agentsList.add(agent);
                 }
 
                 // type index
                 String type = agent.getProperties().get("type");
-                List<AgentEntity> agentsList;
                 if (typeIndex.containsKey(type)) {
                     agentsList = typeIndex.get(type);
                 } else {
@@ -41,6 +64,8 @@ public class Registry {
                     typeIndex.put(type, agentsList);
                 }
                 agentsList.add(agent);
+
+                // all
                 agents.add(agent);
             }
         }
@@ -51,6 +76,28 @@ public class Registry {
     }
 
     public AgentEntity getRandomByType(String type) {
+        List<AgentEntity> agents = typeIndex.get(type);
+        int size = agents.size();
+        if (size == 0) {
+            return null;
+        }
+
+        int randomIndex = new Random().nextInt(size);
+        return agents.get(randomIndex);
+    }
+
+    public AgentEntity getRandomLocalByType(String type) {
+        List<AgentEntity> agents = typeIndex.get(type);
+        int size = agents.size();
+        if (size == 0) {
+            return null;
+        }
+
+        int randomIndex = new Random().nextInt(size);
+        return agents.get(randomIndex);
+    }
+
+    public AgentEntity getRandomForeignByType(String type) {
         List<AgentEntity> agents = typeIndex.get(type);
         int size = agents.size();
         if (size == 0) {
